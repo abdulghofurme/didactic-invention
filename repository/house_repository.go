60de058edd3fe4684ab22724329bcfc6b3a0f950
Abdul Ghofur <abdul.ghofur@bareksa.com>
2023-11-18 08:32:13 +0700

    refactor(houses): refactor query

--- repository/house_repository.go
+++ repository/house_repository.go
@@ -11,7 +11,7 @@ type HouseRepository interface {
 	Create(ctx context.Context, tx *sql.Tx, house domain.House) domain.House
 	Update(ctx context.Context, tx *sql.Tx, house domain.House) domain.House
 	Delete(ctx context.Context, tx *sql.Tx, house domain.House)
-	FindByID(ctx context.Context, tx *sql.Tx, houseId string) (domain.House, error)
-	FindByBlockNumber(ctx context.Context, tx *sql.Tx, houseBlock string, houseNumber int) (domain.House, error)
+	FindByID(ctx context.Context, tx *sql.Tx, houseId string) domain.House
+	FindByBlockNumber(ctx context.Context, tx *sql.Tx, houseBlock string, houseNumber int) domain.House
 	FindAll(ctx context.Context, tx *sql.Tx) []domain.House
 }
